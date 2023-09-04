2be3de9c19042f06dc601bf7aa786f84c2813f4b
Derek Kweon <kdh4646@hanmail.net>
2023-09-03 19:19:41 -0700

    2023-09-03 split components

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const AddUser = () => {
+  const [] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
